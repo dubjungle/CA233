e11673d1198f85bb117c947d2889d48926e28fad
dubjungle <146137369+dubjungle@users.noreply.github.com>
2024-03-08 21:10:26 +0000

    New file manager

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,10 +16,44 @@ public class Company {
     private String companyName;
     private ArrayList <Employee> staff;
 
-// i) Default constructor
+//  Default constructor
 public Company(){
 this.companyName = "Default Company";
 this.staff = new ArrayList<>();
 }
 
+// Overload constructor with a name paramter
+
+public Company(String copmpanyName){
+this.companyName = companyName;
+this.staff = new ArrayList<>();
 }
+
+// Method to add a new employee to the staff arrayList
+public void addNewStaff(Employee employee){
+staff.add(employee);
+
+}
+// Method to lost emplyees above a given employee number value
+public int getStaffNumber(){
+    return staff.size();
+}
+public void listEmployees (int employeeNumberThreshold){
+    System.out.println("Employees with employee number above" + employeeNumberThreshold + ":");
+Iterator<Employee> iterator = staff.iterator();
+
+while(iterator.hasNext()){
+Employee employee = iterator.next();
+if(employee.getEmpNum()> employeeNumberThreshold){
+    System.out.println(employee.getName());}
+}
+
+
+
+}
+
+
+}
+
+
+
