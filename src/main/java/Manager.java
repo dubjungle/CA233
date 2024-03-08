a94b5faae8d2e4168fe47b3232c6db99a698b492
dubjungle <146137369+dubjungle@users.noreply.github.com>
2024-03-08 21:11:15 +0000

    Added script

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -8,6 +8,38 @@
  *
  * @author pc
  */
-public class Manager {
+public class Manager extends Employee {
+    private String username;
+    private String password;
     
+    public Manager (String name, String email, int empNum, String username, String password){
+    
+    super(name,email,empNum);
+    this.username = username;
+    this.password = password;
+    
+    
+    }
+    
+    
+    public String getUsername(){
+    return username;
+   }
+    
+    public void setUsername(String username){
+    this.username = username;}
+    
+    
+    public String getPassword(){
+    return password;}
+    
+    
+    public void setPassword (String password){
+    this.password = password;
+    }
 }
+
+
+
+
+
